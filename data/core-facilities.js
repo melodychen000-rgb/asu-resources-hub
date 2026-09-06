24acf05fa30d0081e15d5d976b3cb655bf0ecbe5
melodychen000-rgb <melodychen000@gmail.com>
2026-09-05 21:14:35 -0700

    Create core-facilities.js

---
+++ data/core-facilities.js
@@ -0,0 +1,86 @@
+export const coreFacilitiesResources = [
+  {
+    name: "Electron Microscopy Center (SEM / TEM)",
+    college: "Core Facilities",
+    campus: "Tempe",
+    location: "Bateman Physical Sciences H-Wing",
+    system_type: "iLab",
+    cost: "Fee-based",
+    eligibility: "Trained Researchers",
+    prerequisites: "EH&S Safety + iLab Account Certification",
+    direct_url: "https://asu.corefacilities.org/",
+    tags: "sem, tem, electron microscope, imaging, nano, eyring"
+  },
+  {
+    name: "Atomic Force Microscope (AFM)",
+    college: "Core Facilities",
+    campus: "Tempe",
+    location: "Goldwater Center (GWC)",
+    system_type: "iLab",
+    cost: "Fee-based",
+    eligibility: "Certified AFM Operators",
+    prerequisites: "AFM Operator Certification Exam",
+    direct_url: "https://asu.corefacilities.org/",
+    tags: "afm, surface metrology, atomic force, nanoscale"
+  },
+  {
+    name: "X-Ray Diffraction Facility (XRD)",
+    college: "Core Facilities",
+    campus: "Tempe",
+    location: "Goldwater Center (GWC)",
+    system_type: "iLab",
+    cost: "Fee-based",
+    eligibility: "Materials Researchers",
+    prerequisites: "Radiation Safety Course + iLab Account",
+    direct_url: "https://asu.corefacilities.org/",
+    tags: "xrd, x-ray diffraction, crystal structure, materials"
+  },
+  {
+    name: "NMR Spectroscopy Center (400/500/600 MHz)",
+    college: "Core Facilities",
+    campus: "Tempe",
+    location: "Physical Sciences D-Wing",
+    system_type: "iLab",
+    cost: "Fee-based",
+    eligibility: "Trained Chemistry Researchers",
+    prerequisites: "High Magnetic Field Safety + Practical Exam",
+    direct_url: "https://asu.corefacilities.org/",
+    tags: "nmr, spectroscopy, chemistry, molecular structure"
+  },
+  {
+    name: "Mass Spectrometry Facility (GC-MS / LC-MS)",
+    college: "Core Facilities",
+    campus: "Tempe",
+    location: "Physical Sciences D-Wing",
+    system_type: "iLab",
+    cost: "Fee-based",
+    eligibility: "Chemistry & Biochemistry Researchers",
+    prerequisites: "Sample Submission Form + PI SpeedType Account",
+    direct_url: "https://asu.corefacilities.org/",
+    tags: "mass spectrometry, gcms, lcms, chemical analysis"
+  },
+  {
+    name: "Laser Scanning Confocal Microscope",
+    college: "Core Facilities",
+    campus: "Tempe",
+    location: "Biodesign Institute B",
+    system_type: "iLab",
+    cost: "Fee-based",
+    eligibility: "Trained Imaging Users",
+    prerequisites: "Basic Microscopy Course + Core Staff Approval",
+    direct_url: "https://asu.corefacilities.org/",
+    tags: "confocal, fluorescence, imaging, cells"
+  },
+  {
+    name: "Biodesign Cryo-Electron Microscopy Suite (Cryo-TEM)",
+    college: "Core Facilities",
+    campus: "Tempe",
+    location: "Biodesign Building C",
+    system_type: "iLab",
+    cost: "Fee-based",
+    eligibility: "Structural Biologists & Protein Crystallographers",
+    prerequisites: "Cryo-TEM Sample Prep Course + Staff Approval",
+    direct_url: "https://asu.corefacilities.org/",
+    tags: "cryo-tem, cryo-em, structural biology, protein imaging"
+  }
+];
