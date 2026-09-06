521151ef0f4df2167a85c64876350c53e7833bed
melodychen000-rgb <melodychen000@gmail.com>
2026-09-05 21:08:13 -0700

    Create academic-support.js

---
+++ data/academic-support.js
@@ -0,0 +1,98 @@
+export const academicSupportResources = [
+  {
+    name: "ASU Writing Centers (All Campuses & Online)",
+    college: "Academic Support Network",
+    campus: "Tempe / Online",
+    location: "Noble Library 280 / All Campuses / Online",
+    system_type: "TutorSearch",
+    cost: "Free",
+    eligibility: "All Enrolled ASU Students (Undergrad & Grad)",
+    prerequisites: "1-on-1 Appointment via TracCloud/TutorSearch or Walk-in",
+    direct_url: "https://tutoring.asu.edu/",
+    tags: "writing center, tutor, tutoring, essay, paper, proofreading, citation, english, dissertation, resume, online"
+  },
+  {
+    name: "ASU Academic Support Network (General Tutoring & Math)",
+    college: "Academic Support Network",
+    campus: "Tempe / Online",
+    location: "Bateman Physical Sciences H-Wing / Online",
+    system_type: "TutorSearch",
+    cost: "Free",
+    eligibility: "All ASU Students",
+    prerequisites: "Log in with ASURITE to view drop-in schedules",
+    direct_url: "https://tutoring.asu.edu/",
+    tags: "tutoring, tutor, math, calculus, algebra, statistics, chemistry, biology, uasp, asn, online"
+  },
+  {
+    name: "Hayden Library Makerspace 3D Printers & Laser",
+    college: "ASU Library",
+    campus: "Tempe",
+    location: "Hayden Library Lower Level 1",
+    system_type: "Walk-in",
+    cost: "Free",
+    eligibility: "All ASU Students",
+    prerequisites: "In-Person Safety Orientation Attendance",
+    direct_url: "https://lib.asu.edu/makerspace",
+    tags: "makerspace, 3d print, laser cutter, sewing, library, hayden, free"
+  },
+  {
+    name: "Noble Library Makerspace Electronics Soldering Benches",
+    college: "ASU Library",
+    campus: "Tempe",
+    location: "Noble Science Library 1F",
+    system_type: "Walk-in",
+    cost: "Free",
+    eligibility: "All ASU Students",
+    prerequisites: "In-Person Soldering Safety Orientation",
+    direct_url: "https://lib.asu.edu/makerspace",
+    tags: "soldering, electronics, noble library, arduino, makerspace"
+  },
+  {
+    name: "Hayden Library Audio Production / Voiceover Studio",
+    college: "ASU Library",
+    campus: "Tempe",
+    location: "Hayden Library Lower Level 1",
+    system_type: "LibCal",
+    cost: "Free",
+    eligibility: "All ASU Students",
+    prerequisites: "LibCal Room Reservation + Library Tech Check-in",
+    direct_url: "https://libcal.asu.edu/",
+    tags: "audio, voiceover, recording booth, library, podcast, soundproof, libcal"
+  },
+  {
+    name: "Student Laptop & Wi-Fi Hotspot Long-Term Checkout",
+    college: "ASU Library",
+    campus: "Tempe",
+    location: "Library Tech Concierge Desks",
+    system_type: "Walk-in",
+    cost: "Free",
+    eligibility: "All Enrolled ASU Students",
+    prerequisites: "Sun Card + Overdue Financial Agreement",
+    direct_url: "https://lib.asu.edu/services/borrow",
+    tags: "laptop checkout, hotspot, library lending, tech concierge"
+  },
+  {
+    name: "SDFC Climbing & Bouldering Wall",
+    college: "SDFC",
+    campus: "Tempe",
+    location: "SDFC Climbing Center",
+    system_type: "Walk-in",
+    cost: "Free",
+    eligibility: "All Enrolled ASU Students",
+    prerequisites: "Climbing Wall Safety Waiver + Belay Certification",
+    direct_url: "https://fitness.asu.edu/facilities/climbing-wall",
+    tags: "rock climbing, bouldering, belay, recreation, sdfc, fitness"
+  },
+  {
+    name: "SDFC Aquatic Center Lap Swim Reserved Lanes",
+    college: "SDFC",
+    campus: "Tempe",
+    location: "Mona Plummer Aquatic Center",
+    system_type: "Walk-in",
+    cost: "Free",
+    eligibility: "Enrolled Students & Registered Lap Swimmers",
+    prerequisites: "Valid Sun Card Scan at Gate",
+    direct_url: "https://fitness.asu.edu/facilities/aquatics",
+    tags: "lap swimming, pool, aquatic center, recreation, sdfc"
+  }
+];
